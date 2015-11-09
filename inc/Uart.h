80c4af673318a95fad920a923334ad0d565c643f
vortex314 <lieven.merckx@gmail.com>
2015-11-09 20:21:57 +0100

    update

--- inc/Uart.h
+++ inc/Uart.h
@@ -19,21 +19,13 @@ class Uart : public Stream {
 public:
 	virtual ~Uart(){};
 	virtual void init(uint32_t baudrate)=0;
-<<<<<<< HEAD
-=======
-	virtual void init(uint8_t bits,uint8_t parity,uint8_t stopBits);
->>>>>>> ba2e932e81ca0f0e0ca5b746c6e6bf126d9e3403
+^//	virtual void init(uint8_t bits,uint8_t parity,uint8_t stopBits)=0;
 	virtual Erc write(Bytes& data)=0;
 	virtual Erc write(uint8_t b)=0;
 	virtual bool hasData()=0;
 	virtual bool hasSpace()=0;
 	virtual uint8_t read()=0;
 	virtual void receive(uint8_t b)=0;
-<<<<<<< HEAD
-=======
-
-
->>>>>>> ba2e932e81ca0f0e0ca5b746c6e6bf126d9e3403
 };
 
 #endif /* UART_H_ */
