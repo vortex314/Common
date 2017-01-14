c26dd9f2e667a81a1a57fed710e9d31744bf3ad8
vortex314 <lieven.merckx@gmail.com>
2017-01-14 12:31:50 +0100

    replace Log by logger to avoid log(double)

--- inc/Log.h
+++ inc/Log.h
@@ -22,7 +22,7 @@ public:
 	typedef enum  { LOG_TRACE=0,LOG_DEBUG,LOG_INFO,LOG_WARN,LOG_ERROR,LOG_FATAL } LogLevel;
 private:
 	bool _enabled;
-	LogFunction _logFunction;
+	LogFunction _loggerFunction;
 	char _hostname[20];
 	char _application[20];
 	LogLevel _level;
@@ -45,30 +45,30 @@ public:
 	LogLevel level();
 };
 
-extern Log log;
+extern Log loggerger;
 
 #ifdef ARDUINO
 #define nullptr 0
 #include <Arduino.h>
 
 #define __FLE__ strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__
-#define LOGF(fmt,...)  if ( log.enabled(Log::LOG_FATAL)) {log.printf("%lu | %s:%d | ", millis(),__PRETTY_FUNCTION__,__LINE__);log.printf(fmt,##__VA_ARGS__);log.flush();}//delay(10);
-#define ASSERT_LOG(xxx) if ( !(xxx)) { log.printf(" Assertion failed %s",#xxx); log.flush();while(1){delay(1000);};}
-#define ASSERT(xxx) if ( !(xxx)) { log.printf(" Assertion failed %s",#xxx); log.flush();while(1){delay(1000);};}
+#define LOGF(fmt,...)  if ( logger.enabled(Log::LOG_FATAL)) {logger.printf("%lu | %s:%d | ", millis(),__PRETTY_FUNCTION__,__LINE__);logger.printf(fmt,##__VA_ARGS__);logger.flush();}//delay(10);
+#define ASSERT_LOG(xxx) if ( !(xxx)) { logger.printf(" Assertion failed %s",#xxx); logger.flush();while(1){delay(1000);};}
+#define ASSERT(xxx) if ( !(xxx)) { logger.printf(" Assertion failed %s",#xxx); logger.flush();while(1){delay(1000);};}
 
 #else
 
-#define LOGF(fmt,...)  {log.time();log.host(0);log.application(0);log.printf("| %s:%d  ",__PRETTY_FUNCTION__,__LINE__);log.printf(fmt,##__VA_ARGS__);log.flush();}//delay(10);
-#define ASSERT_LOG(xxx) if ( !(xxx)) { log.printf(" Assertion failed %s",#xxx); log.flush();while(1){Sys::delay(1000);};}
-#define ASSERT(xxx) if ( !(xxx)) { log.printf(" Assertion failed %s",#xxx); log.flush();while(1){Sys::delay(1000);};}
+#define LOGF(fmt,...)  {logger.time();logger.host(0);logger.application(0);logger.printf("| %s:%d  ",__PRETTY_FUNCTION__,__LINE__);logger.printf(fmt,##__VA_ARGS__);logger.flush();}//delay(10);
+#define ASSERT_LOG(xxx) if ( !(xxx)) { logger.printf(" Assertion failed %s",#xxx); logger.flush();while(1){Sys::delay(1000);};}
+#define ASSERT(xxx) if ( !(xxx)) { logger.printf(" Assertion failed %s",#xxx); logger.flush();while(1){Sys::delay(1000);};}
 
 #endif
 
-#define INFO(fmt,...)  if ( log.enabled(Log::LOG_INFO)) LOGF(fmt,##__VA_ARGS__)
-#define ERROR(fmt,...)  if ( log.enabled(Log::LOG_ERROR)) LOGF(fmt,##__VA_ARGS__)
-#define WARN(fmt,...)  if ( log.enabled(Log::LOG_WARN)) LOGF(fmt,##__VA_ARGS__)
-#define FATAL(fmt,...)  if ( log.enabled(Log::LOG_FATAL)) LOGF(fmt,##__VA_ARGS__)
-#define DEBUG(fmt,...)  if ( log.enabled(Log::LOG_DEBUG)) LOGF(fmt,##__VA_ARGS__)
-#define TRACE(fmt,...)  if ( log.enabled(Log::LOG_TRACE)) LOGF(fmt,##__VA_ARGS__)
+#define INFO(fmt,...)  if ( logger.enabled(Log::LOG_INFO)) LOGF(fmt,##__VA_ARGS__)
+#define ERROR(fmt,...)  if ( logger.enabled(Log::LOG_ERROR)) LOGF(fmt,##__VA_ARGS__)
+#define WARN(fmt,...)  if ( logger.enabled(Log::LOG_WARN)) LOGF(fmt,##__VA_ARGS__)
+#define FATAL(fmt,...)  if ( logger.enabled(Log::LOG_FATAL)) LOGF(fmt,##__VA_ARGS__)
+#define DEBUG(fmt,...)  if ( logger.enabled(Log::LOG_DEBUG)) LOGF(fmt,##__VA_ARGS__)
+#define TRACE(fmt,...)  if ( logger.enabled(Log::LOG_TRACE)) LOGF(fmt,##__VA_ARGS__)
 
 #endif /* LOG_H_ */
