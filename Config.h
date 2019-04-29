aa02249a3d7695df46af6a1d3320917227e08541
vortex314 <lieven.merckx@gmail.com>
2019-04-29 22:58:45 +0200

    adapted to ArduinoJson v6

--- Config.h
+++ Config.h
@@ -13,44 +13,45 @@
 #include <ArduinoJson.h>
 #include <Log.h>
 
-class Config {
-		void initialize();
-		void initMagic();
-		bool checkMagic();
-		char _charBuffer[1024];
-		DynamicJsonBuffer _jsonBuffer;
-		JsonObject* _root;
+class Config
+{
+    void initialize();
+    void initMagic();
+    bool checkMagic();
+    char _charBuffer[1024];
+    DynamicJsonDocument _jsonBuffer;
+    JsonObject _root;
 
-		std::string _nameSpace;
-		bool _loaded;
+    std::string _nameSpace;
+    bool _loaded;
 
-		JsonObject& nameSpace();
-		const char* clone(const char* s);
+    JsonObject nameSpace();
+//   const char* clone(const char* s);
 
-	public:
-		Config();
-		virtual ~Config();
+public:
+    Config();
+    virtual ~Config();
 
-		void clear();
-		void load();
-		void save();
-		void print(std::string& str);
-		void printPretty(std::string& str);
+    void clear();
+    void load();
+    void save();
+    void print(std::string& str);
+    void printPretty(std::string& str);
 
-		bool hasKey(const char* key);
-		void setNameSpace(const char* ns);
-		const char* getNameSpace();
-		void get(const char*, int32_t&, int32_t defaultValue);
-		void get(const char*, uint32_t&, uint32_t defaultValue);
-		void get(const char*, std::string&, const char* defaultValue);
-		void get(const char*, double&, double defaultValue);
-		void remove(const char* key);
+    bool hasKey(const char* key);
+    void setNameSpace(const char* ns);
+    const char* getNameSpace();
+    void get(const char*, int32_t&, int32_t defaultValue);
+    void get(const char*, uint32_t&, uint32_t defaultValue);
+    void get(const char*, std::string&, const char* defaultValue);
+    void get(const char*, double&, double defaultValue);
+    void remove(const char* key);
 
-		void set(const char*, uint32_t);
-		void set(const char*, int32_t);
-		void set(const char*, std::string&);
-		void set(const char*, double);
-		void set(const char* key, const char* value);
+    void set(const char*, uint32_t);
+    void set(const char*, int32_t);
+    void set(const char*, std::string&);
+    void set(const char*, double);
+    void set(const char* key, const char* value);
 };
 
 extern Config config;
