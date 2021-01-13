ba7bccceb3a14a21ad693b1cd0d2084529666e32
lieven <lieven.merckx@gmail.com>
2021-01-13 01:04:31 +0100

    CircBuf issue

--- CircBuf.cpp
+++ CircBuf.cpp
@@ -10,7 +10,7 @@
 #include "Sys.h"
 
 // start[writePos] => location of next write
-// start[readPos] => location of last read 
+// start[readPos] => location of next read
 
 CircBuf::CircBuf(int size) {
     start = new uint8_t[size];
@@ -27,41 +27,47 @@ void CircBuf::clear() {
 
 CircBuf::~CircBuf() { delete[] start; }
 
-int CircBuf::write(uint8_t b) { // not in  as it will be called in interrupt
-    if ((writePos - readPos) == limit)
+int CircBuf::write(uint8_t b) {
+    if ((writePos - readPos) >= (limit - 1))
         return -EAGAIN;
     //	Board::disableInterrupts();
-    start[writePos++ % limit] = b;
+    start[writePos % limit] = b;
+    writePos++;
     //	Board::enableInterrupts();
     return 0;
 }
 
 int CircBuf::writeFromIsr(uint8_t b) {
-    if ((writePos - readPos) == limit)
+    if ((writePos - readPos) >= (limit - 1))
         return -EAGAIN;
-    start[writePos++ % limit] = b;
+    start[writePos % limit] = b;
+    writePos++;
     return 0;
 }
 
 int CircBuf::readFromIsr() {
-    if (readPos < writePos)
-        return start[++readPos % limit];
+    if (readPos < writePos) {
+        readPos++;
+        return start[(readPos - 1) % limit];
+    }
     return -1;
 }
 
 int CircBuf::read() {
     //		Board::disableInterrupts();
-    if (readPos < writePos)
-        return start[++readPos % limit];
+    if (readPos < writePos) {
+        readPos++;
+        return start[(readPos - 1) % limit];
+    }
     return -1;
     //		Board::enableInterrupts();
 }
 
-uint32_t CircBuf::size() { return writePos - readPos; }
+uint32_t CircBuf::size() { return (writePos - readPos); }
 
 uint32_t CircBuf::space() { return limit - size(); }
 
-bool CircBuf::hasSpace() { return size() < limit; }
+bool CircBuf::hasSpace() { return size() < (limit - 1); }
 
 bool CircBuf::hasSpace(uint32_t sz) { return size() < sz; }
 
